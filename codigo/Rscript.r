9add236d2242ea5eec492ea0df0462dfb0234a76
Cristian Londoño Franco <clondonof@unal.edu.co>
2021-12-05 17:19:28 -0500

    New db
    se descargo la nueva bd y se realizo la limpieza + agrupamiento

--- codigo/Rscript.r
+++ codigo/Rscript.r
@@ -4,51 +4,116 @@ rm(list = ls())
 
 
 #dataframe
-acc_med <- read_delim("GitHub/AccidentalidadEnMedellin/incidentes_viales.csv",
+acc_med <- read_delim("GitHub/AccidentalidadEnMedellin/incidentes_viales.csv", locale = locale(encoding = "UTF-8"),
                       ";", escape_double = FALSE, col_types = cols(`AÑO` = col_character(),
                                                                    FECHA_ACCIDENTES = col_character()),
                       trim_ws = TRUE)
 #limpieza
 acc_med$AÑO[acc_med$AÑO == "2019\\r"] <- "2019"
-
 acc_med$CLASE_ACCIDENTE[acc_med$CLASE_ACCIDENTE %in% c("Caída de Ocupante","Caida Ocupante", "Caída Ocupante")] <- "Caida de Ocupante"
+acc_med$COMUNA <- gsub("\\xC1", "Á", acc_med$COMUNA, fixed = TRUE)
+acc_med$COMUNA <- gsub("\\xF3", "ó", acc_med$COMUNA, fixed = TRUE)
+acc_med$COMUNA <- gsub("\\xED", "í", acc_med$COMUNA, fixed = TRUE)
+acc_med$COMUNA <- gsub("\\xF1", "ñ", acc_med$COMUNA, fixed = TRUE)
+acc_med$COMUNA <- gsub("\\xFA", "ú", acc_med$COMUNA, fixed = TRUE)
+acc_med$COMUNA <- gsub("\\xE1", "á", acc_med$COMUNA, fixed = TRUE)
+acc_med$COMUNA <- gsub("\\xE9", "é", acc_med$COMUNA, fixed = TRUE)
+acc_med$BARRIO <- gsub("\\xC1", "Á", acc_med$BARRIO, fixed = TRUE)
+acc_med$BARRIO <- gsub("\\xF3", "ó", acc_med$BARRIO, fixed = TRUE)
+acc_med$BARRIO <- gsub("\\xED", "í", acc_med$BARRIO, fixed = TRUE)
+acc_med$BARRIO <- gsub("\\xF1", "ñ", acc_med$BARRIO, fixed = TRUE)
+acc_med$BARRIO <- gsub("\\xFA", "ú", acc_med$BARRIO, fixed = TRUE)
+acc_med$BARRIO <- gsub("\\xE1", "á", acc_med$BARRIO, fixed = TRUE)
+acc_med$BARRIO <- gsub("\\xE9", "é", acc_med$BARRIO, fixed = TRUE)
+acc_med$GRAVEDAD_ACCIDENTE <- gsub("\\xF1", "ñ", acc_med$GRAVEDAD_ACCIDENTE, fixed = TRUE)
 
-colnames(acc_med)[colnames(acc_med) == "MES"] <- "DAÑOS" ##cambio nombre de columna
-acc_med$DAÑOS[acc_med$DAÑOS == "Solo da\\xF1os"] <- "Solo daños"
-
-##por facilidad trabajar con las localidades
-##correcion de los nombres de algunas localidades
-acc_med$LOCATION[acc_med$LOCATION %in% c("Bel\\xE9n", "Belén")] <- "Belen"
-acc_med$LOCATION[acc_med$LOCATION %in% c("Corregimiento de San Crist\\xF3bal", "Corregimiento de San Cristóbal")] <- "Corregimiento de San Cristobal"
-acc_med$LOCATION[acc_med$LOCATION %in% c("Corregimiento de San Sebasti\\xE1n de Palmitas", "Corregimiento de San Sebastián de Palmitas")] <- "Corregimiento de San Sebastian de Palmitas"
-acc_med$LOCATION[acc_med$LOCATION %in% c("La Am\\xE9rica", "La América")] <- "La America"
-
-##eliminando datos inecesarios
-acc_med <- acc_med[!(acc_med$LOCATION %in% c("SN","Sin Inf","AU","In", "0", NA)), ]
-
+##creaccion de fecha
 acc_med <- cbind(acc_med, 
-                 strcapture('(\\d*[/]\\d*[/]\\d*)', 
+                 strcapture('(\\d*[/]\\d*[/]\\d*)',
                             acc_med$FECHA_ACCIDENTE, data.frame(FECHA = character())))
 
 #dbExport <- data.frame(FECHA = acc_med$FECHA, CLASE = acc_med$CLASE_ACCIDENTE, DANOS = acc_med$DAÑOS, LOCACION = acc_med$LOCATION)
 #write.csv2(dbExport, 'db-acc-med.csv')
 
-#agrupamiento por barrio
+#bd map
 barrios <- st_read("GitHub/AccidentalidadEnMedellin/shp/Barrio_Vereda_2014.shp")
-barrios <- barrios[ , 3:8]
-colnames(barrios)[colnames(barrios) == "NOMBRE"] <- "BARRIO"
+barrios <- barrios[ , c(3,5,6,7,8)]
 
-acc_med2 <- acc_med[ , c(1,3,4,6,10,15,16,17,18)]
-colnames(acc_med2)[colnames(acc_med2) == "LOCATION"] <- "BARRIO"
+#criterios Clase_acc, gravedad, tipo via, frecuencia
 
-#criterios Clase_acc, daños, frecuencia
-frecuencia <- data.frame(table(acc_med2$BARRIO, acc_med2$CLASE_ACCIDENTE, acc_med2$DAÑOS))
-colnames(frecuencia)<-c("BARRIO","CLASE_ACC","DAÑOS","FRECUENCIA")
+##frecuencia de acc por barrio
+freq <- data.frame(table(acc_med$BARRIO))
+map1 <- merge(x=freq, y=barrios, by.y="NOMBRE", by.x="Var1")
 
-#crear mapa
+##freccuencia de acc por clase de accidente
+freq_class = data.frame(table(acc_med$CLASE_ACCIDENTE, acc_med$BARRIO))
+map2.1 <- merge(x=freq_class[freq_class$Var1=="Atropello", ], y=barrios, by.y="NOMBRE", by.x="Var2")
+map2.2 <- merge(x=freq_class[freq_class$Var1=="Caida de Ocupante", ], y=barrios, by.y="NOMBRE", by.x="Var3")
+map2.3 <- merge(x=freq_class[freq_class$Var1=="Choque", ], y=barrios, by.y="NOMBRE", by.x="Var2")
+map2.4 <- merge(x=freq_class[freq_class$Var1=="Incendio", ], y=barrios, by.y="NOMBRE", by.x="Var2")
+map2.5 <- merge(x=freq_class[freq_class$Var1=="Volcamiento", ], y=barrios, by.y="NOMBRE", by.x="Var2")
+map2.6 <- merge(x=freq_class[freq_class$Var1=="Otro", ], y=barrios, by.y="NOMBRE", by.x="Var2")
 
-com <- barrios[barrios$NOMBRE_COM == "El Poblado", ]
+##frecuencia de acc en tipos de via
+freq_vias = data.frame(table(acc_med$DISEÑO,acc_med$BARRIO))
+
+##frecuencia de graveda de acc
+freq_grav = data.frame(table(acc_med$GRAVEDAD_ACCIDENTE, acc_med$BARRIO))
+
+##Combinanciones
+
+
+#mapas
+##general
+ggplot() +
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map1, aes(geometry = geometry, fill=Freq))+
+  ggtitle("Frecuencia de Accidentes en Medellin")
+##por clases de acc
+ggplot() +
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map2.1, aes(geometry = geometry, fill=Freq))+
+  scale_fill_gradientn(colours = c("#FF3030", "red4"))+
+  ggtitle("Frecuencia de Atropellos")
 
 ggplot() +
-  geom_sf(data = barrios,fill="white", color = "black")+
-  geom_sf(data = com, fill = "pink")
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map2.2, aes(geometry = geometry, fill=Freq))+
+  scale_fill_gradientn(colours = c("#FF3030", "red4"))+
+  ggtitle("Frecuencia de Caidas de ocupante")
+
+ggplot() +
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map2.3, aes(geometry = geometry, fill=Freq))+
+  scale_fill_gradientn(colours = c("#FF3030", "red4"))+
+  ggtitle("Frecuencia de Choques")
+
+ggplot() +
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map2.4, aes(geometry = geometry, fill=Freq))+
+  scale_fill_gradientn(colours = c("#FF3030", "red4"))+
+  ggtitle("Frecuencia de Incendios")
+
+ggplot() +
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map2.5, aes(geometry = geometry, fill=Freq))+
+  scale_fill_gradientn(colours = c("#FF3030", "red4"))+
+  ggtitle("Frecuencia de VOlcamientos")
+ggplot() +
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map2.6, aes(geometry = geometry, fill=Freq))+
+  scale_fill_gradientn(colours = c("#FF3030", "red4"))+
+  ggtitle("Frecuencia de Otros Accidentes")
+
+##tipos de vias
+ggplot() +
+  geom_sf(data =medellin ,fill="white", color = "black")+
+  scale_fill_gradientn(colours = c("#1E90FF", "blue4"))+
+  geom_sf(data = ACCbarrios[ACCbarrios$CLASE_ACC=="Caida de Ocupante", ], aes(geometry = geometry, fill=FRECUENCIA))
+
+##por graveda
+ggplot() +
+  geom_sf(data =barrios ,fill="white", color = "black")+
+  geom_sf(data = map2.1, aes(geometry = geometry, fill=Freq))+
+  scale_fill_gradientn(colours = c("#FF3030", "red4"))+
+  ggtitle("Frecuencia de Atropellos")
